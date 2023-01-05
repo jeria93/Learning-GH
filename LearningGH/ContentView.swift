69f896898201fddd393e05743b2183c40b8f0e77
Nicholas <nicholasjobb.jeria@gmail.com>
2023-01-05 09:52:32 +0100

    Initial Commit

---
+++ LearningGH/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  LearningGH
+//
+//  Created by Nicholas Gaete Jeria on 2023-01-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+        VStack {
+            
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            
+            Text("Hello, world!")
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Nicholas Gaete Jeria on 2023-01-05.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
