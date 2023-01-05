0c0bd2c9ada87371d3fd16e34862efee7fc6d014
Nicholas <nicholasjobb.jeria@gmail.com>
2023-01-05 18:03:49 +0100

    Add struct  to new project

---
+++ LearningGH/MyModel.swift
@@ -0,0 +1,15 @@
+//
+//  MyModel.swift
+//  LearningGH
+//
+//  Created by Nicholas Gaete Jeria on 2023-01-05.
+//
+
+import Foundation
+
+struct MyModel {
+    
+    var name: String?
+    
+    
+}
